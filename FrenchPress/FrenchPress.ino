8f632ebc3a48f1460f894908998f21020cb278da
Peter Bull <pjbull@gmail.com>
2012-09-08 18:15:21 -0400

    1. Improved 7 segment display logic and data structures. 2. Added all pins as variables. 3. Added useful time constants in ms as variables. Currently set to test values. 4. Updated brew logic. 5. Updated header.

--- FrenchPress/FrenchPress.ino
+++ FrenchPress/FrenchPress.ino
@@ -1,175 +1,173 @@
 /*
-  Blink
- Turns on an LED on for one second, then off for one second, repeatedly.
+  FrenchPress
+ Controls an automatic French Press coffee maker. Stirs after first minute,
+ counts down time, and plays a tune a brew completion.
  
- This example code is in the public domain.
+ See http://peterbull.me/tt_fp.html for details on the construction
+ and Fritzing circuit diagram.
+ 
+ Prereqs:
+ Add 205 F water and freshly ground coffee beans. Press start button.
+ 
+ This code performs the following steps:
+ 1. Set 7segment display to 4. 
+ 2. Wait one minute.
+ 3. Set 7 segment display to 3.
+ 3. Begin stirring coffee by triggering magnetic stirrer mechanism.
+ 4. Stir for 30s.
+ 5. Wait 30s.
+ 6. Set 7 segment display to 2.
+ 7. Wait one minute.
+ 8. Set 7 segment display to 1.
+ 9. Wait one minute.
+ 10. Set 7 segment display to 0.
+ 11. Play brew complete melody.
+ 12. Turn on green brew comeplete LED.
+ 13. Wait 1 minute.
+ 14. Turn off LEDs.
+ 
+ Postreqs:
+ Press french press. Pour coffee. Enjoy a masterful brew.
+ 
+ (CC BY-SA 3.0) Peter Bull
  */
 
- #include "pitches.h"
+// 7 segment LED to pin mapping
+byte top = 2; //      top
+byte tl = 3;  //      ---
+byte tr = 4;  //  tl |   | tr
+byte mid = 5; //  mid ---  
+byte bl = 6;  //  bl |   | br
+byte br = 7;  //      ---
+byte bot = 8; //      bot
 
-// notes in the melody:
-int melody[] = {
-  //NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
-  NOTE_A3, NOTE_A3, NOTE_G3, NOTE_A3, NOTE_G3, NOTE_F3, NOTE_G3, NOTE_E3};
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = {
-  1, 1, 8, 4, 2, 4, 4, 2 };
+// Tells the arduino which pins to turn on for which digits.
+byte one[] =   {tr, br};
+byte two[] =   {top, tr, mid, bl, bot};
+byte three[] = {top, tr, mid, br, bot};
+byte four[] =  {tl, tr, mid, br};
+byte five[] =  {top, tl, mid, br, bot};
+byte six[] =   {tl, mid, bl, br, bot};
+byte seven[] = {top, tr, br};
+byte eight[] = {top, tl, tr, mid, bl, br, bot};
+byte nine[] =  {top, tl, tr, mid, br};
+byte zero[] =  {top, tl, tr, bl, br, bot};
+
+// The 7segment digit to display is equal to its index in this array
+byte* digits[] = {zero, one, two, three, four, five, six, seven, eight, nine};
+
+// Piezo output pin
+byte piezoPin = 11;
+#include "pitches.h"
+
+// Bob Dylan's "One More Cup of Coffee" (the chorus, more or less...)
+int melody[] = {NOTE_A3, NOTE_A3, NOTE_G3, NOTE_A3, NOTE_G3, NOTE_F3, NOTE_G3, NOTE_E3};
+int noteDurations[] = {1, 1, 8, 4, 2, 4, 4, 2 };
+
+// Other pins
+byte startButtonPin = 12;   // Controls the momentary button which starts the brew process.
+byte greenLedPin = 13;      // Contols the green brew finished LED
+byte stirTransistorPin = 9; // Controls the transistor which gates the stirring mechanism
+
+// Time intervals
+int oneMinuteMs =  (1 *1000);
+int halfMinuteMs = (3 *1000);
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT); 
-  pinMode(4, OUTPUT); 
-  pinMode(5, OUTPUT); 
-  pinMode(6, OUTPUT); 
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT); 
-  pinMode(9, OUTPUT); 
-  pinMode(11, OUTPUT);
-  pinMode(12, INPUT);
+  // initialize the 7 segment display pins as output.
+  for (byte ledPin = 0; ledPin < sizeof(digits[8]); ledPin++)
+  {
+    pinMode(ledPin, OUTPUT);
+  }
+  
+  // initialize the other pins
+  pinMode(piezoPin, OUTPUT);
+  pinMode(startButtonPin, INPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(stirTransistorPin, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-    int buttonState = digitalRead(12);
+  int buttonState = digitalRead(startButtonPin);
 
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {     
-    // turn LED on:    
+  if (buttonState == HIGH) {       
     brew();
-    //playDone();
-    //playDone();
-    //playDone();
   } 
 }
 
 void brew() {
+  // Start countdown
   displayValue(4);
-  delay(3000);
+  delay(oneMinuteMs);
   
+  // Stir for half a minute
   displayValue(3);
-  digitalWrite(9, HIGH);
-  delay(5000);
-  digitalWrite(9, LOW);
-  delay(5000);
+  digitalWrite(stirTransistorPin, HIGH);
+  delay(halfMinuteMs);
   
+  // Stop stirring
+  digitalWrite(stirTransistorPin, LOW);
+  delay(halfMinuteMs);
+
+  // Countdown
   displayValue(2);
-  delay(5000);
+  delay(oneMinuteMs);
   displayValue(1);
-  delay(5000);
+  delay(oneMinuteMs);
+  
+  // Brew finished
   displayValue(0);
   playDone();
+  digitalWrite(greenLedPin, HIGH);
+  delay(oneMinuteMs);
+  
+  // turn off LEDs
+  digitalWrite(greenLedPin, LOW);
   displayValue(-1);
 }
 
 
 void playDone() {
   // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8 ; thisNote++) {
+  for (int thisNote = 0; thisNote < (sizeof(melody)/sizeof(int)) ; thisNote++) {
 
     // to calculate the note duration, take one second 
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000/noteDurations[thisNote];
-    tone(11, melody[thisNote], noteDuration);
+    tone(piezoPin, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
-    noTone(11);
+    noTone(piezoPin);
   }
 }
 
 void displayValue(int i)
 {
-  // turn off all LEDs
-  for (int cnt = 2; cnt < 9; cnt++)
+  // turn off all LEDs by setting pins in the digit 8 to LOW
+  for (byte ledPin = 0; ledPin < sizeof(digits[8]); ledPin++)
   {
-    digitalWrite(cnt, LOW);
+    digitalWrite(ledPin, LOW);
   }
-
-  if(i==1)
+  
+  // if it is a single digit, set the appropriate pins to HIGH
+  if(i > -1 && i < 10)
   {
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
+    for (byte ledPin = 0; ledPin < sizeof(digits[i]); ledPin++)
+    {
+      digitalWrite(ledPin, HIGH);
+    }
   }
-  else if(i == 2)
-  {
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-  }
-  else if(i == 3)
-  {
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(8, HIGH);
-  }
-  else if(i == 4)
-  {
-    digitalWrite(7, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(8, HIGH);
-  }
-  else if(i == 5)
-  {
-    digitalWrite(6, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(8, HIGH);
-  }
-  else if(i == 6)
-  {
-    digitalWrite(3, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(8, HIGH);
-  }
-  else if(i == 7)
-  {
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-  }
-  else if(i == 8)
-  {
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(8, HIGH);
-  }
-  else if(i == 9)
-  {
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(8, HIGH);
-  }
-  else if(i == 0)
-  {
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(8, HIGH);
-  }
-
+  // else 7 segment display is off
 }
+
 
 
